e323dd599602e60395518c6e184d2b864b98c060
Prabinjha <prabinjha2074@gmail.com>
2021-03-08 16:18:14 +0000

    input and print

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print( len( input("what is your name? " ) ) )
+a = input("a: ")
+b = input("b: ")
+print("Real value of a is " + a)
+print("Real value of b is " + b)
+
+c = a
+a = b
+b = c
+print("Exchanged value of a is " + a)
+print("Exchanged value of b is " + b)
+
+
+
