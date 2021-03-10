23d731eb3d1709731a12c01a00afe5a765a3fcad
Prabinjha <prabinjha2074@gmail.com>
2021-03-10 07:26:47 +0000

    first day

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-a = input("a: ")
-b = input("b: ")
-print("Real value of a is " + a)
-print("Real value of b is " + b)
+# a = input("a: ")
+# b = input("b: ")
+# print("Real value of a is " + a)
+# print("Real value of b is " + b)
 
-c = a
-a = b
-b = c
-print("Exchanged value of a is " + a)
-print("Exchanged value of b is " + b)
+# c = a
+# a = b
+# b = c
+# print("Exchanged value of a is " + a)
+# print("Exchanged value of b is " + b)
 
-
+# create a greetings for your program.
+print("Welcome to the band name generator.")
+city = input("Enter the city you born ?\n")
+pet = input("Enter name of your pet?\n")
+print("Your Brand name can be " + city +" " + pet)
 
